cf316731a483142ae0e75eb927a735073ab334c0
Eduar Ursu <eduardursu78@gmail.com>
2021-08-22 19:20:10 +0200

    Update Toolz homepage

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,27 @@
+    
+    function navbar() {
+        var t = this;
+        t.n = document.querySelector("nav");
+        t.bo = document.body.style.overflow;
+        t.close = function () {
+          t.bo = "auto";
+          t.n.classList.remove("active");
+        }
+        t.open = function () {
+          t.bo = "hidden";
+          t.n.classList.add("active");
+        }
+        if (t.n) {
+          document.querySelector("nav>button").addEventListener("click", function () {
+            console.log("toggleNav");
+            if (t.n.classList.contains("active"))
+              t.close();
+            else
+              t.open();
+          });
+          document.querySelectorAll("nav ul > a").forEach(n => n.addEventListener("click", t.close()));
+        }
+      }
     function themeManager() {
       const t = this;
       t.toggles = document.getElementsByClassName("theme-toggle");
@@ -83,6 +107,7 @@
 
   // Call the function when the DOM is loaded
   document.addEventListener("DOMContentLoaded", () => {
+      new navbar();
       new themeManager();
       new gotop();
       new aos();
