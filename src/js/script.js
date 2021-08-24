8364693d7ea45673e700d6a1866fc31e114e68ec
Eduar Ursu <eduardursu78@gmail.com>
2021-08-25 00:43:01 +0200

    Tools design improvements

--- src/js/script.js
+++ src/js/script.js
@@ -104,11 +104,37 @@
           t.listeners();
       }
   }
+  function modal(id) {
+    var t = this;
+    t.m = document.querySelector((id) ? id : '.modal');
+    if (t.m) {
+        t.bdy = document.body.classList;
+        t.targets = document.querySelectorAll('[data-toggle="' + t.m.id + '"]');
+        t.closebtns = t.m.querySelectorAll('.close-modal');
+    }
+    t.show = function () {
+        t.bdy.add('_overflowhidden');
+        t.m.classList.add('_show-modal');
+    }
+    t.hide = function () {
+        t.m.classList.remove('_show-modal');
+        t.bdy.remove('_overflowhidden');
+    }
+    t.listeners = function () {
+        t.targets.forEach(el => {
+            el.addEventListener('click', function (e) {
+                t.show();
+            });
+        });
+        t.closebtns.forEach(function (item) {
+            item.addEventListener('click', function (e) {
+                t.hide();
+            });
+        });
+    }
+    if (t.m)
+        t.listeners();
+}
 
-  // Call the function when the DOM is loaded
-  document.addEventListener("DOMContentLoaded", () => {
-      new navbar();
-      new themeManager();
-      new gotop();
-      new aos();
-  });
+
+ 
